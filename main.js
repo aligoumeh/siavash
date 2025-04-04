d9c3d7bfa580495d0c4669a47b63db42968d1161
aliG <ali.goumeh@gmail.com>
2025-04-04 23:10:07 +0200

    fonts and style updated

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 // main.js
-// Add smooth scrolling for nav links
+// Add smooth scrolling for nav links\
+
+const video = document.querySelector('.video-container video');
+document.addEventListener('DOMContentLoaded', function() { // Wait for DOM to load
+    video.play().catch(error => {
+        console.error("Autoplay prevented:", error);
+        // Show a play button or other fallback
+        const playButton = document.createElement('button');
+        playButton.textContent = 'Play Video';
+        playButton.addEventListener('click', () => {
+           video.play();
+        });
+        document.querySelector('.video-container').appendChild(playButton);
+
+    });
+});
 document.querySelectorAll('nav a').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
     e.preventDefault();
@@ -9,3 +24,11 @@ document.querySelectorAll('nav a').forEach(anchor => {
     });
   });
 });
+
+function changeImage(imageSrc, text) {
+  const mainImage = document.getElementById('mainImage');
+  const itemText = document.querySelector('.title');
+  
+  mainImage.src = imageSrc;
+  itemText.innerText = text; // Update title with the clicked item's description
+}
